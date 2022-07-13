336642978c27a384d9903fdd5270ef59d582d32d
Eisuke Yoshida <ekusie.yeongjo@outlook.com>
2022-07-13 21:19:07 +0900

    20220713 Add variables

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+  type     = string
+  nullable = false
+}
+
+variable "aws_secret_key" {
+  type     = string
+  nullable = false
+}
+
+variable "region" {
+  type     = string
+  nullable = false
+}
+
